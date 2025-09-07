8a744e502fea2c11bb1be950ff1e8b8fc666304c
Sniperer <clutchself@gmail.com>
2025-09-07 20:15:48 +0200

    fix pdf forward and backward

--- init.el
+++ init.el
@@ -36,6 +36,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+'(region ((t (:background "LightGoldenrod2" :foreground "black"))))
+ '(hl-line ((t (:background "gray20"))))
  )
 (load "~/.emacs.d/pdf.el")
 ;;(load "~/.emacs.d/languagetool.el")
